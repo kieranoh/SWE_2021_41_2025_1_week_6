e2ecc037fce0da37eb80ec9d7717c6249b7dc37b
kieranoh <77526407+kieranoh@users.noreply.github.com>
2025-04-09 18:40:04 +0900

    Add files via upload

---
+++ arrange.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+for file in "files"/*; do
+    if [ -f "$file" ]; then
+        filename=$(basename "$file")
+        first_char=$(echo "${filename:0:1}" | tr '[:upper:]' '[:lower:]')
+        if [[ $first_char =~ [a-z] ]]; then
+            mv "$file" "$first_char"
+        fi
+    fi
+done
